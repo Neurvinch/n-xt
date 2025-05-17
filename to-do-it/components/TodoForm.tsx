6c364646bfb7e782224b9916b077f87567a30ccd
Naveen pandian <npandian515@gmail.com>
2025-05-17 20:15:36 +0530

    Add TodoForm component with basic structure and props interface

---
+++ to-do-it/components/TodoForm.tsx
@@ -0,0 +1,12 @@
+import React,{useState, FormEvent} from 'react'
+
+interface TodoFormProps {
+    onAdd: (text: string) => void;
+}
+const TodoForm = () => {
+  return (
+    <div>TodoForm</div>
+  )
+}
+
+export default TodoForm
