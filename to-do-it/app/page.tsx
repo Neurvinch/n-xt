c5ef7216fb2d3b1ad560ef6e7407fdcea1c74ef9
Naveen pandian <npandian515@gmail.com>
2025-05-17 20:32:26 +0530

    Add Todo interface and initialize state for todos in Home component

--- to-do-it/app/page.tsx
+++ to-do-it/app/page.tsx
@@ -2,7 +2,24 @@
 import { useState, useEffect } from "react";
 
 
+interface Todo {
+  id: number;
+  text : string;
+}
+
 export default function Home() {
+
+   const [todos,setTodos] = useState<Todo[]>([]);
+
+    useEffect( () => {
+      const storedTodos =  localStorage.getItem("todos")
+
+      if(storedTodos) {
+        setTodos( JSON.parse(storedTodos))
+      }
+    },[])
+
+    
   return (
     <>
     </>
