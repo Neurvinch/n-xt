d35f94e0f12326bf4ffe46a8f63ea0eeb3b1fe0b
Naveen pandian <npandian515@gmail.com>
2025-05-17 20:18:52 +0530

    Refactor TodoForm component to include props and handle form submission

--- to-do-it/components/TodoForm.tsx
+++ to-do-it/components/TodoForm.tsx
@@ -3,7 +3,19 @@ import React,{useState, FormEvent} from 'react'
 interface TodoFormProps {
     onAdd: (text: string) => void;
 }
-const TodoForm = () => {
+const TodoForm = ({onAdd} : TodoFormProps) => {
+
+    const [text, setText] = useState(" ");
+    
+    const handleSubmit = ( e: FormEvent) => {
+         
+        e.preventDefault();
+
+        if(text.trim()) {
+            onAdd(text);
+            setText("");
+        }
+    }
   return (
     <div>TodoForm</div>
   )
